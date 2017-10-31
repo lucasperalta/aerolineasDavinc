babea7f780abeaea87ee44dd7ce1e1150268f629
Lucas <lucas@C1MNT7R0DTY3.local>
2017-10-31 00:15:15 -0300

    commit inicial

---
+++ src/main/java/com/davinci/aerolineas/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.davinci.aerolineas.dao;
+
+import java.util.List;
+
+import com.davinci.aerolineas.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
